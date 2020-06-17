a496a0403f0fe1248756981208eb1f5f8a8c549f
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-18 02:02:03 +0530

    Added, edit person details

--- src/com/bridgelabz/addressbook/AdressBook.java
+++ src/com/bridgelabz/addressbook/AdressBook.java
@@ -5,11 +5,31 @@ import java.util.Scanner;
 public class AdressBook {
     public static void main(String[] args) {
         System.out.println("\tWelcome to Adress Book");
-        ArrayList<PersonDetails> list = new ArrayList<>();
+//        ArrayList<PersonDetails> list = new ArrayList<>();
         PersonDetails person = new PersonDetails("Nikhil","Dhruwe","lingampally","Hyderabad","Telangana",500032,"789654123");
-        person.addPerson();
+       // person.addPerson();
         //System.out.println(list);
-        person.print();
+        //person.print();
+        Scanner scan = new Scanner(System.in);
+        boolean status=true;
+        while(status==true)
+        {
+            System.out.println("Select : \n 1: Add Person details \n2: Edit \n3: Display \n4: Exit");
+            int num=scan.nextInt();
+            switch (num)
+            {
+                case 1 : person.addPerson();
+                    break;
+                case 2: person.editPerson();
+                    break;
+                case 3 : person.print();
+                    break;
+                case 4 :status=false;
+                    break;
+                default:
+                    System.out.println("Invalid choise");
 
+            }
+        }
     }
 }
--- src/com/bridgelabz/addressbook/PersonDetails.java
+++ src/com/bridgelabz/addressbook/PersonDetails.java
@@ -1,3 +1,4 @@
+
 package com.bridgelabz.addressbook;
 import java.util.Scanner;
 public class PersonDetails {
@@ -22,6 +23,63 @@ public class PersonDetails {
         this.zip=zip;
         this.phone=phone;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public void addPerson()
     {
         System.out.println("\nTo add person");
@@ -48,6 +106,17 @@ public class PersonDetails {
         System.out.print("Enter Phone Number: ");
         this.phone=scan.next();
         //list.add(this.addPerson());
+    }
+    public void editPerson()
+    {
+        System.out.println("Enter Name");
+        String name=scan.next();
+        if (this.name.equals(name))
+        {
+            addPerson();
+        }
+        else
+            System.out.println("Name does not match");
     }
     public void print()
     {
