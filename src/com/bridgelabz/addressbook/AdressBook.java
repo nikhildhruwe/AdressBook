3e8cb863e057a8f6280435a012b0d09ad6e4d10c
nikhildhruwe <nick.dhruwe@gmail.com>
2020-06-27 00:55:23 +0530

    Refactored: Replaced compare method with lambda

--- src/com/bridgelabz/addressbook/AdressBook.java
+++ src/com/bridgelabz/addressbook/AdressBook.java
@@ -1,49 +1,43 @@
 package com.bridgelabz.addressbook;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdressBook {
     public static void main(String[] args) {
         System.out.println("\tWelcome to Adress Book");
-        PersonComputation personComputation = new PersonComputation();
-        //***** Menu for operations on Array List*****//
+
+        PersonComputation personComputation=new PersonComputation();
+        // Menu for operations on Array List
         Scanner scan = new Scanner(System.in);
-        boolean status = true;
-        while (status) {
+        boolean status=true;
+        while(status==true)
+        {
             System.out.println("\nSelect : \n1: Add Person details \n2: Delete \n3: Display \n4: Edit" +
-                    " \n5: Sort by Name \n6: Sort by city,State or Zip \n7: Person by City&State \n8:" +
-                    " View Person by City/State \n9: Exit");
-            int num = scan.nextInt();
-            switch (num) {
-                case 1:
-                    personComputation.addPerson();
+                    " \n5: Sort by Name \n6: Sort by city,State or Zip \n7: Person by City&State \n8: View Person by City/State \n9: Exit");
+            int num=scan.nextInt();
+            switch (num)
+            {
+                case 1 : personComputation.addPerson();
                     break;
-                case 2:
-                    personComputation.deletePerson();
+                case 2: personComputation.deletePerson();
                     break;
-                case 3:
-                    personComputation.print();
+                case 3 : personComputation.print();
                     break;
-                case 4:
-                    personComputation.editPerson();
+                case 4 : personComputation.editPerson();
                     break;
-                case 5:
-                    personComputation.sortByName();
+                case 5 : personComputation.sortByName();
                     break;
-                case 6:
-                    personComputation.sortByCityStateZip();
+                case 6: personComputation.sortByCityStateZip();
                     break;
-                case 7:
-                    personComputation.viewPersonCityState();
+                case 7 : personComputation.viewPersonCityState();
                     break;
-                case 8:
-                    personComputation.cityOrState();
+                case 8 : personComputation.cityOrState();
                     break;
-                case 9:
-                    status = false;
+                case 9 :status=false;
                     break;
                 default:
                     System.out.println("Invalid choice");
+
             }
         }
     }
